9daee70be9887e0e7c2227640718bb78bc5ea66f
junnn0021 <junnn0021@gmail.com>
2023-09-11 12:24:31 +0900

    [create] was-tomcat folder cicd test

---
+++ was-tomcat/init-database.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Initializing MySQL database..."
+mysql -h 10.190.64.3 -u user -pqwe123!@# <<EOF
+CREATE DATABASE IF NOT EXISTS petclinic;
+EOF
+
+echo "Adding sample data to the database..."
+mysql -h 10.190.64.3 -u user -pqwe123!@# petclinic < /docker-entrypoint-initdb.d/data.sql
+
+echo "Database initialization and data insertion complete."
