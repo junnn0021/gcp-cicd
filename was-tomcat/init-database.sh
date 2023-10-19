49ba1ecd51076e55f8a4e1213402cc3da3d00249
junnn0021 <junnn0021@gmail.com>
2023-10-19 15:56:11 +0900

    practice

--- was-tomcat/init-database.sh
+++ was-tomcat/init-database.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Initializing MySQL database..."
-mysql -h 10.190.64.3 -u user -pqwe123!@# <<EOF
+mysql -h 10.232.48.3 -u user -pqwe123!@# <<EOF
 CREATE DATABASE IF NOT EXISTS petclinic;
 EOF
 
 echo "Adding sample data to the database..."
-mysql -h 10.190.64.3 -u user -pqwe123!@# petclinic < /docker-entrypoint-initdb.d/data.sql
+mysql -h 10.232.48.3 -u user -pqwe123!@# petclinic < /docker-entrypoint-initdb.d/data.sql
 
 echo "Database initialization and data insertion complete."
